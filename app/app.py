f3d64810b33540e571d390d6063de160b70d306d
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-01-08 09:43:03 +0200

    fix

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+    return '<h1>Hello from Flask & Elestio</h1>'
+
+@app.route('/templates')
+def template():
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
